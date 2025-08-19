22145b4334321b387b571d405dc8b98fd983ebe0
Srdjan Lukic <srdjan@lukic.nu>
2025-08-19 09:34:20 +0200

    opgave

---
+++ opgave.py
@@ -0,0 +1,27 @@
+import requests
+
+# Funktion til at hente en joke fra en API
+
+# Funktion til at hilse på brugeren
+
+# Hovedprogrammet
+def main():
+    print("Hello world")
+    # Indtastning af brugerens navn og alder
+ 
+
+    # Indtastning af en liste af hobbyer eller farver
+
+
+    # If-else logik baseret på alder
+
+    # Udskriv en velkomsthilsen
+
+
+    # Vis brugerens hobbyer eller farver
+  
+    # Hent og vis en joke fra API’et
+
+
+# Kør hovedprogrammet
+main()
